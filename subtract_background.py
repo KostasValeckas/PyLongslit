6671c0324f10005119436dd602eb9e2be0407bca
KostasValeckas <kostas.valeckas@nbi.ku.dk>
2025-01-21 17:24:03 +0100

    file saving and preliminary tests on background subtraction

--- subtract_background.py
+++ subtract_background.py
@@ -32,10 +32,12 @@ def subtract_background(reduced_files):
 
     # load the images
     images = {}
+    headers = {}
     for file in reduced_files:
         with open_fits(output_dir, file) as hdul:
             new_filename = file.replace('reduced_science_', '').replace('reduced_std_', '')
             images[new_filename] = hdul[0].data
+            headers[new_filename] = hdul[0].header
 
     subtracted_images = {}
     for i in range(len(file_pairs)):
@@ -65,6 +67,20 @@ def subtract_background(reduced_files):
         # Display the plot
         plt.tight_layout()
         plt.show()
+
+    # save the subtracted images
+    for filename in reduced_files:
+        new_filename = filename.replace('reduced_science_', '').replace('reduced_std_', '')
+        if new_filename in subtracted_images:
+            image = subtracted_images[new_filename]
+            header = headers[new_filename]
+            logger.info(f"Saving subtracted image {filename} to disc.")
+            write_to_fits(image, header, filename, output_dir)
+        
+
+    logger.info("Subtracted images saved to disc.")
+
+    
         
         
 
