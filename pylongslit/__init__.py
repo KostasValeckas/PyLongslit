a2c8e4deb1dffd14dbb551ba34d9b0285d3f079a
KostasValeckas <kostas.valeckas@nbi.ku.dk>
2025-02-25 08:39:46 +0100

    __init__.py cleanup done

--- pylongslit/__init__.py
+++ pylongslit/__init__.py
@@ -1,3 +1,8 @@
+"""
+This module is called any time the package is initialized. Only functtion
+it has is to set the path to the config file.
+"""
+
 CONFIG_FILE_PATH = "config.json"  # Default config file path
 
 def set_config_file_path(path):
